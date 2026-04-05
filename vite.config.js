8cc980dcfa0c5de2e1828bc09a7dc1c1bec3c52e
andrewwProgerCool <andreyleskovka@gmail.com>
2026-04-05 04:40:51 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/polynom-lab-ti/',
+  base: '/TI_Lab_2/',
   plugins: [
     vue(),
     vueDevTools(),
